31976a8fe701b8a2ab9a2324f1576bdecc9bed90
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2023-05-12 10:06:41 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 # A puppet manuscript to replace a line in a file on a server
-
 $file_to_edit = '/var/www/html/wp-settings.php'
-
 #replace line containing "phpp" with "php"
 
 exec { 'replace_line':
