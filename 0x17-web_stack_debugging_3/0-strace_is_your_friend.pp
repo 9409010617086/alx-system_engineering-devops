ac889242bd60d19b7bf4cf310ec74036fe11c8c4
pretty mohlabe <111237261+9409010617086@users.noreply.github.com>
2023-05-14 18:53:32 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # Fixing the 500 error when a GET HTTP method is requested to Apache web server
-
 exec {'replace':
 	provider => shell,
 	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
